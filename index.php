86e5f1cdd2666298984e275dac8773e34814188f
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-06-20 20:02:48 -0300

    save

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
 <?php
-include "../PapelariaNovoMundo/includes/cabecalho.php";
-include "../PapelariaNovoMundo/includes/carrossel.php";
+include "../ProjetoIntegrador/includes/cabecalho.php";
 ?>
+<div id="carouselExampleIndicators" class="carousel slide">
+<div class="carousel-indicators ">
+  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+</div>
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="img/aulas.png" class="d-block w-100" alt="...">
+  </div>
+  <div class="carousel-item">
+    <img src="img/materiais.png" class="d-block w-100" alt="...">
+  </div>
+  <div class="carousel-item">
+    <img src="img/promocao.png" class="d-block w-100" alt="...">
+  </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon bg-dark-subtle" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <span class="carousel-control-next-icon bg-dark-subtle" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+</div>
 
 
 <?php
-include "../PapelariaNovoMundo/includes/rodape.php";
+include "../ProjetoIntegrador/includes/rodape.php";
 ?>
