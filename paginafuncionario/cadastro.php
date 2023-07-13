069dbd08e7fe89fd17928fac56163242570f26d8
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-07-13 19:09:10 -0300

    .

---
+++ paginafuncionario/cadastro.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <title>Login Papelaria Novo Mundo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<div style="font-family:cursive">
+<form class="formulario form-control w-50 position-absolute top-50 start-50 translate-middle">
+<h1 class="text-center mb-5">Cadastrar novo funcionário </h1>
+
+
+<div class="container text-center">
+<div class="row row-cols-2">
+<div>
+<label  class="form-label fw-bold">Nome</label>
+<input type="emial" class="form-control" placeholder="Nome completo">
+</div>
+
+<div>
+    <label  class="form-label fw-bold">Funcionalidade</label>
+    <input type="emial" class="form-control" placeholder="Função">
+    </div>
+
+<div>
+<label  class="form-label fw-bold ">Email</label>
+<input type="emial" class="form-control" placeholder="papelaria@gmail.com">
+</div>
+
+<div> 
+<label  class="form-label fw-bold">Rua</label>
+<input type="emial" class="form-control" placeholder="">
+</div>
+
+<div>
+<label  class="form-label fw-bold">Senha</label>
+<input type="password" class="form-control" placeholder="*********">
+</div>
+
+<div>
+<label  class="form-label fw-bold">Cidade</label>
+<input type="emial" class="form-control" placeholder="">
+</div>
+
+<div>
+<label  class="form-label fw-bold">CEP</label>
+<input type="number" class="form-control" placeholder="00000-000" maxlength="8">
+</div>
+
+<div>
+<label  class="form-label fw-bold">Telefone</label>
+<input type="number" class="form-control" placeholder="(00) 00000-0000">
+</div>
+
+<div>
+<label  class="form-label fw-bold">CPF</label>
+<input type="number" class="form-control" placeholder="00000-000" maxlength="8">
+</div>
+
+<div>
+<label  class="form-label fw-bold">Estado</label>
+<input type="emial" class="form-control" placeholder="">
+</div>
+
+</div>
+</div>
+<br>
+<div class="d-flex justify-content-center">
+<button >Cadastrar</button>
+</div>
+
+</form>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+</body>
+</html>
