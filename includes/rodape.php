e962da9623f63bc1736768bed4082e04a4da6032
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-07-31 22:11:05 -0300

    .

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,11 +1,10 @@
 <footer class="bg-gradient rodape me-0" style=" margin-bottom: -10%; height:40px;">
 <p style="margin-top:11%;text-align:center; font-size:80%;">
-&copy; Todos os direitos reservados. Criado pelos alunos do Senac - Geovana, Guilherme, Henrique, Pedro Santos
+&copy; Todos os direitos reservados. Criado pelos alunos do Senac - Geovana, Guilherme, Henrique e Pedro Santos
 </p>
 </footer>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-<script src="../pagina/script.js"></script>
 </body>
 </html>
